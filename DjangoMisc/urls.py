e0c844b7bbe7e63382bdcc1db2dad69770927268
Jihad Dayoub <pythoner1991@gmail.com>
2023-02-16 13:43:03 +0300

    Github simple integration

--- DjangoMisc/urls.py
+++ DjangoMisc/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from gh import views as gh_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # gh
+    path('', gh_views.index),
 ]
